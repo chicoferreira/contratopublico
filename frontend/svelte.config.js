d7aafd963c30a8bae147d6c3fadac39970adb77f
chicoferreira <36338391+chicoferreira@users.noreply.github.com>
2025-08-11 00:12:36 +0100

    Revert `svelte.config.ts` to `svelte.config.js`

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,8 +1,8 @@
-import type { Config } from "@sveltejs/kit";
 import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ".md"],
   preprocess: [
@@ -16,6 +16,6 @@ const config = {
   kit: {
     adapter: adapter(),
   },
-} as Config;
+};
 
 export default config;
